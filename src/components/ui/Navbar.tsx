c8f1a454d8df5d3f93f63a3d5baf4c90f3d36088
amogustroll69 <usr.bin.ducc.2@gmail.com>
2024-03-29 01:31:20 +0300

    feat: title easter egg

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -6,13 +6,54 @@ import { ThemeSwitch } from "@/components/ui/ThemeSwitch";
 import { MusicToggle } from "@/components/ui/MusicToggle";
 import { useState } from "react";
 
+const titleEasterEggs = [
+  ":3",
+  "UwU",
+  "uwu",
+  "-w-",
+  "OwO",
+  "owo",
+  "-.-",
+  "O.O",
+  "o.o",
+  "//_//",
+  "//.//",
+  "@.@",
+  "@-@",
+  "^w^",
+  "^-^",
+  "~w~",
+  "^.^",
+  ":)",
+  ":D",
+  ":P",
+  ":S",
+  ":(",
+  ":-(",
+  ";-;",
+  ":o",
+  "(^_^)",
+  "(v_v)",
+  "<333",
+  "T_T",
+  "XD",
+  "xx)",
+  "x_x",
+  "^_^",
+  "<3",
+  "//w//",
+];
+
 export const Navbar: React.FC = () => {
   const [isTitleHovered, setIsTitleHovered] = useState(false);
+  const [currentTitleEasterEgg, setCurrentTitleEasterEgg] = useState(
+    titleEasterEggs[Math.floor(Math.random() * titleEasterEggs.length)],
+  );
 
   return (
     <>
       <DesktopBrowser>
-        <nav className="flex items-center justify-between mx-16 my-8">
+        <nav className="flex items-center justify-between ml-20 mr-12 my-8">
           <div className="flex items-center">
             <Button className="mr-3">Random</Button>
 
@@ -23,22 +64,41 @@ export const Navbar: React.FC = () => {
           <div className="flex items-center">
             <HOS className="size-12 mr-3 rounded-lg" />
 
-            <div
-              className="relative"
-              onMouseEnter={() => setIsTitleHovered(!isTitleHovered)}
-              onMouseLeave={() => setIsTitleHovered(!isTitleHovered)}
-            >
-              <p
-                className={`text-4xl blur-[10px] cursor-default ${isTitleHovered ? "animate-gradient-faster" : "animate-gradient"}`}
+            <div className="flex items-center">
+              <div
+                className="relative mr-8"
+                onMouseEnter={() => {
+                  setIsTitleHovered(!isTitleHovered);
+                  setCurrentTitleEasterEgg(
+                    titleEasterEggs[
+                      Math.floor(Math.random() * titleEasterEggs.length)
+                    ],
+                  );
+                }}
+                onMouseLeave={() => setIsTitleHovered(false)}
               >
-                <strong>sqd's hall of shame</strong>
-              </p>
+                <p className="text-4xl blur-[10px] select-none animate-gradient">
+                  <strong>sqd's hall of shame</strong>
+                </p>
 
-              <p
-                className={`absolute inset-0 z-10 text-4xl cursor-default ${isTitleHovered ? "animate-gradient-faster" : "animate-gradient"}`}
-              >
-                <strong>sqd's hall of shame</strong>
-              </p>
+                <p className="absolute inset-0 z-10 text-4xl select-none animate-gradient">
+                  <strong>sqd's hall of shame</strong>
+                </p>
+              </div>
+
+              <div className="relative">
+                <p
+                  className={`w-10 blur-[10px] mt-0.5 text-[#eb5be8] text-lg select-none transition-opacity duration-150 ${isTitleHovered ? "opacity-100" : "opacity-0"}`}
+                >
+                  <strong>{currentTitleEasterEgg}</strong>
+                </p>
+
+                <p
+                  className={`absolute inset-0 z-10 w-10 mt-0.5 text-[#eb5be8] text-lg select-none transition-opacity duration-150 ${isTitleHovered ? "opacity-100" : "opacity-0"}`}
+                >
+                  <strong>{currentTitleEasterEgg}</strong>
+                </p>
+              </div>
             </div>
           </div>
         </nav>
