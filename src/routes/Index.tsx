c532f710801706729ff74a8ce2e1493e091d285c
amogustroll69 <usr.bin.ducc.2@gmail.com>
2024-03-28 18:36:21 +0300

    fix: remove virus

--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -2,31 +2,23 @@ import { Spinner } from "@/components/ui/Spinner";
 import { DesktopBrowser } from "@/components/ui/DesktopBrowser";
 import { MobileBrowser } from "@/components/ui/MobileBrowser";
 import { Navbar } from "@/components/ui/Navbar";
-import { useState } from "react";
 
 export const Index: React.FC = () => {
-  const [virusMoment, setVirusMoment] = useState(false);
-
   return (
     <>
-      <Navbar
-        onVirusMoment={() => setVirusMoment(true)}
-        onVirusMomentClose={() => setVirusMoment(false)}
-      />
+      <Navbar />
 
-      <div className={virusMoment ? "blur-sm" : ""}>
-        <DesktopBrowser>
-          <div className="absolute-center">
-            <Spinner />
-          </div>
-        </DesktopBrowser>
+      <DesktopBrowser>
+        <div className="absolute-center">
+          <Spinner />
+        </div>
+      </DesktopBrowser>
 
-        <MobileBrowser>
-          <div className="absolute-center">
-            <Spinner />
-          </div>
-        </MobileBrowser>
-      </div>
+      <MobileBrowser>
+        <div className="absolute-center">
+          <Spinner />
+        </div>
+      </MobileBrowser>
     </>
   );
 };
