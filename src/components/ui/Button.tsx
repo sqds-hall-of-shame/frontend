259be43ac4fcf7feded9094d871eb690dacc60a1
amogustroll69 <usr.bin.ducc.2@gmail.com>
2024-03-30 01:49:35 +0300

    feat: wider button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -9,7 +9,7 @@ export const Button: React.FC<Props> = (props: Props) => (
   <button
     type={props.type || "button"}
     onClick={props.onClick}
-    className={`p-2 rounded-[10px] select-none transition border-2 border-transparent text-white bg-white bg-opacity-[7%] hover:bg-black hover:bg-opacity-[25%] dark:bg-opacity-[10%] dark:hover:bg-white dark:hover:bg-opacity-[20%] dark:border-2 dark:border-transparent ${props.className || ""}`}
+    className={`py-2 px-4 rounded-[10px] select-none transition border-2 border-transparent text-white bg-white bg-opacity-[7%] hover:bg-black hover:bg-opacity-[25%] dark:bg-opacity-[10%] dark:hover:bg-white dark:hover:bg-opacity-[20%] dark:border-2 dark:border-transparent ${props.className || ""}`}
   >
     {props.children}
   </button>
