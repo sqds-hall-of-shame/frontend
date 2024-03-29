48f20e4ca282fe387e1f8832664d762432dae5fa
amogustroll69 <usr.bin.ducc.2@gmail.com>
2024-03-30 00:30:17 +0300

    feat: better navbar design

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -6,6 +6,7 @@ import { ThemeSwitch } from "@/components/ui/ThemeSwitch";
 import { MusicToggle } from "@/components/ui/MusicToggle";
 import { ScrollbarToggle } from "./ScrollbarToggle";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const titleEasterEggs = [
   ":3",
@@ -75,7 +76,18 @@ export const Navbar: React.FC = () => {
   return (
     <>
       <DesktopBrowser>
-        <div className="mb-[150px]">
+        <div
+          className="mb-[150px]"
+          onMouseEnter={() => {
+            setIsTitleHovered(!isTitleHovered);
+            setCurrentTitleEasterEgg(
+              titleEasterEggs[
+                Math.floor(Math.random() * titleEasterEggs.length)
+              ],
+            );
+          }}
+          onMouseLeave={() => setIsTitleHovered(false)}
+        >
           <div className="flex justify-center items-center fixed top-4 left-1/2 transform -translate-x-1/2 w-[95%] z-50 bg-black bg-opacity-[75%] dark:bg-white dark:bg-opacity-[3%] duration-150 rounded-3xl border border-[#383838] backdrop-filter backdrop-blur-[8px]">
             <nav className="flex items-center text-center justify-between w-full my-8 ml-24">
               <div className="flex items-center">
@@ -86,46 +98,37 @@ export const Navbar: React.FC = () => {
                 <ScrollbarToggle />
               </div>
 
-              <div className="flex items-center">
-                <HOS className="size-12 mr-3 rounded-lg" />
+              <Link to="/">
+                <div className="flex items-center">
+                  <HOS className="size-12 mr-3 rounded-lg" />
 
-                <div className="flex items-center text-center">
-                  <div
-                    className="relative mr-4 text-center"
-                    onMouseEnter={() => {
-                      setIsTitleHovered(!isTitleHovered);
-                      setCurrentTitleEasterEgg(
-                        titleEasterEggs[
-                          Math.floor(Math.random() * titleEasterEggs.length)
-                        ],
-                      );
-                    }}
-                    onMouseLeave={() => setIsTitleHovered(false)}
-                  >
-                    <p className="text-4xl blur-[10px] select-none text-gradient">
-                      <strong>sqd's hall of shame</strong>
-                    </p>
+                  <div className="flex items-center text-center">
+                    <div className="relative mr-4 text-center">
+                      <p className="text-4xl blur-[10px] select-none text-gradient">
+                        <strong>sqd's hall of shame</strong>
+                      </p>
 
-                    <p className="absolute inset-0 z-10 text-4xl select-none text-gradient">
-                      <strong>sqd's hall of shame</strong>
-                    </p>
-                  </div>
+                      <p className="absolute inset-0 z-10 text-4xl select-none text-gradient">
+                        <strong>sqd's hall of shame</strong>
+                      </p>
+                    </div>
 
-                  <div className="relative mr-4">
-                    <p
-                      className={`w-20 blur-[10px] text-[#eb5be8] text-2xl select-none transition-opacity duration-150 ${isTitleHovered ? "opacity-100" : "opacity-0"}`}
-                    >
-                      <strong>{currentTitleEasterEgg}</strong>
-                    </p>
+                    <div className="relative mr-4">
+                      <p
+                        className={`w-20 blur-[10px] text-[#eb5be8] text-2xl select-none transition-opacity duration-150 ${isTitleHovered ? "opacity-100" : "opacity-0"}`}
+                      >
+                        <strong>{currentTitleEasterEgg}</strong>
+                      </p>
 
-                    <p
-                      className={`absolute inset-0 z-10 w-20 text-[#eb5be8] text-lg select-none transition-opacity duration-150 ${isTitleHovered ? "opacity-100" : "opacity-0"}`}
-                    >
-                      <strong>{currentTitleEasterEgg}</strong>
-                    </p>
+                      <p
+                        className={`absolute inset-0 z-10 w-20 text-[#eb5be8] text-lg select-none transition-opacity duration-150 ${isTitleHovered ? "opacity-100" : "opacity-0"}`}
+                      >
+                        <strong>{currentTitleEasterEgg}</strong>
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
+              </Link>
             </nav>
           </div>
         </div>
