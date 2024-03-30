0e20aa4322cbac37741c5a9ef47c74b457376462
amogustroll69 <usr.bin.ducc.2@gmail.com>
2024-03-30 04:13:12 +0300

    chore: remove <br /> and image

--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -1,6 +1,7 @@
 import { DesktopBrowser } from "@/components/ui/DesktopBrowser";
 import { MobileBrowser } from "@/components/ui/MobileBrowser";
 import { Navbar } from "@/components/ui/Navbar";
+import { Spinner } from "@/components/ui/Spinner";
 
 export const Index: React.FC = () => {
   return (
@@ -8,173 +9,14 @@ export const Index: React.FC = () => {
       <Navbar />
 
       <DesktopBrowser>
-        <div className="mx-12">
-          <img src="https://media.discordapp.net/attachments/1205569419649089577/1222565669279760396/image.png?ex=6616ae01&is=66043901&hm=a6ef2ae6f14c545de96b209c2c8cb0e0ae6e13e9855d915b9054a651d558000f&=&format=webp&quality=lossless" />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
+        <div className="absolute-center">
+          <Spinner />
         </div>
       </DesktopBrowser>
 
       <MobileBrowser>
         <div className="mx-3">
-          <img src="https://media.discordapp.net/attachments/1205569419649089577/1222565669279760396/image.png?ex=6616ae01&is=66043901&hm=a6ef2ae6f14c545de96b209c2c8cb0e0ae6e13e9855d915b9054a651d558000f&=&format=webp&quality=lossless" />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
+          <p className="text-black">thing</p>
         </div>
       </MobileBrowser>
     </>
